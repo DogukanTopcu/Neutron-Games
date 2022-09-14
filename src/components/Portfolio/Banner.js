54b1a0e44c315011230f1ad1dcbaf472a4f2dcc6
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-09-14 15:59:09 +0300

    banner and new logo

---
+++ src/components/Portfolio/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./banner.css";
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+        <div className='container banner-content'>
+            <div className='neutron'>Neutron</div>
+            <div className='games'>Games</div>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Banner from '../../components/Portfolio/Banner'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div>
+      <Banner />
+    </div>
   )
 }
 
