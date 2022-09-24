60bdd94485a096b6e6f4b03ebe2702b957ddc014
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-09-24 20:58:03 +0300

    images with scrollMagics for our games

--- src/components/Portfolio/Banner.js
+++ src/components/Portfolio/Banner.js
@@ -4,6 +4,16 @@ import "./banner.css";
 import YellowTriangle from "../../assets/1x/yellow-triangle.png";
 import GreenTriangle from "../../assets/1x/green-triangle.png";
 
+import ANewSun from "../../assets/bannerImages/ANewSun.png";
+import AyCarmela from "../../assets/bannerImages/AyCarmela.png";
+import DontLookDown from "../../assets/bannerImages/DontLookDown.png";
+import EscapeForGalapagos from "../../assets/bannerImages/EscapeForGalapagos.png";
+import GammaLand from "../../assets/bannerImages/GammaLand.png";
+import LittleRedRidingHood from "../../assets/bannerImages/LittleRedRidingHood.png";
+import SoccerRun from "../../assets/bannerImages/SoccerRun.png";
+import ThePatient from "../../assets/bannerImages/ThePatient.png";
+
+
 const Banner = () => {
 
   return (
@@ -17,11 +27,14 @@ const Banner = () => {
             <div className='banner-second-content'>
               <h2>Did someone say Game?</h2>
 
-              {/* <img className='banner-img-format bannerImg-one' src={} />
-              <img className='banner-img-format bannerImg-two' src={} />
-              <img className='banner-img-format bannerImg-three' src={} />
-              <img className='banner-img-format bannerImg-four' src={} />
-              <img className='banner-img-format bannerImg-five' src={} /> */}
+              <img className='banner-img-format bannerImg-one' src={ANewSun} />
+              <img className='banner-img-format bannerImg-two' src={AyCarmela} />
+              <img className='banner-img-format bannerImg-three' src={DontLookDown} />
+              <img className='banner-img-format bannerImg-four' src={EscapeForGalapagos} />
+              <img className='banner-img-format bannerImg-five' src={GammaLand} />
+              <img className='banner-img-format bannerImg-six' src={LittleRedRidingHood} />
+              <img className='banner-img-format bannerImg-seven' src={SoccerRun} />
+              <img className='banner-img-format bannerImg-eight' src={ThePatient} />
 
               <div className='discover-our-games-button'>
                 <p>Discover Our Games</p>
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -23,9 +23,19 @@ const Portfolio = () => {
     let bannerTextOne = document.querySelector(".banner-first-text");
     let bannerContent = document.querySelector(".banner-second-content");
 
+    // Banner Images
+    let imgOne = document.querySelector(".bannerImg-one");
+    let imgTwo = document.querySelector(".bannerImg-two");
+    let imgThree = document.querySelector(".bannerImg-three");
+    let imgFour = document.querySelector(".bannerImg-four");
+    let imgFive = document.querySelector(".bannerImg-five");
+    let imgSix = document.querySelector(".bannerImg-six");
+    let imgSeven = document.querySelector(".bannerImg-seven");
+    let imgEight = document.querySelector(".bannerImg-eight");
+
 
     // Banner magic
-    let bannerFixed = 5000;
+    let bannerFixed = 10000;
     if (bannerSection && window.scrollY <= bannerFixed) {
       bannerSection.style.position = "fixed";
       bannerSection.style.top = "0";
@@ -80,17 +90,111 @@ const Portfolio = () => {
     }
 
 
-    // Banner Content 2
-    if (window.scrollY > 3100) {
+    // Banner Content 2 and Image falling
+    if (window.scrollY > 3100 && window.scrollY < 4500) {
       bannerContent.style.opacity = (window.pageYOffset - 3100) / 350;
     }
     else if (window.scrollY < 3000) {
       bannerContent.style.opacity = 0;
     }
 
+    else if (window.scrollY > 4500 && window.scrollY < 10000) {
+      // img1 anim
+      imgOne.style.display = "block";
+      imgOne.style.opacity = (window.pageYOffset - 4500) / 500;
+      imgOne.style.transform = `translate(${(window.pageYOffset - 4500) / 3}px, ${(window.pageYOffset - 4500) / 2}px)`;
+      imgOne.style.width = `${((window.pageYOffset - 4500) / 3) + 300}px`;
+      imgOne.style.height = `${((window.pageYOffset - 4500) / 3) + 200}px`;
 
+      // img2 anim
+      if(window.scrollY > 5000) {
+        imgTwo.style.display = "block";
+        imgTwo.style.opacity = (window.pageYOffset - 5000) / 500;
+        imgTwo.style.transform = `translate(-${(window.pageYOffset - 5000) / 3}px, ${(window.pageYOffset - 5000) / 2}px)`;
+        imgTwo.style.width = `${((window.pageYOffset - 5000) / 3) + 300}px`;
+        imgTwo.style.height = `${((window.pageYOffset - 5000) / 3) + 200}px`;
+
+        // img3 anim
+        if (window.scrollY > 5500) {
+          imgThree.style.display = "block";
+          imgThree.style.opacity = (window.pageYOffset - 5500) / 500;
+          imgThree.style.transform = `translate(${(window.pageYOffset - 5500) / 3}px, ${(window.pageYOffset - 5500) / 2}px)`;
+          imgThree.style.width = `${((window.pageYOffset - 5500) / 3) + 300}px`;
+          imgThree.style.height = `${((window.pageYOffset - 5500) / 3) + 200}px`;
+
+          // img4 anim
+          if (window.scrollY > 6000) {
+            imgFour.style.display = "block";
+            imgFour.style.opacity = (window.pageYOffset - 6000) / 500;
+            imgFour.style.transform = `translate(-${(window.pageYOffset - 6000) / 3}px, ${(window.pageYOffset - 6000) / 2}px)`;
+            imgFour.style.width = `${((window.pageYOffset - 6000) / 3) + 300}px`;
+            imgFour.style.height = `${((window.pageYOffset - 6000) / 3) + 200}px`;
+
+            // img5 anim
+            if (window.scrollY > 6500) {
+              imgFive.style.display = "block";
+              imgFive.style.opacity = (window.pageYOffset - 6500) / 500;
+              imgFive.style.transform = `translate(${(window.pageYOffset - 6500) / 3}px, ${(window.pageYOffset - 6500) / 2}px)`;
+              imgFive.style.width = `${((window.pageYOffset - 6500) / 3) + 300}px`;
+              imgFive.style.height = `${((window.pageYOffset - 6500) / 3) + 200}px`;
+
+              // img6 anim
+              if (window.scrollY > 7000) {
+                imgSix.style.display = "block";
+                imgSix.style.opacity = (window.pageYOffset - 7000) / 500;
+                imgSix.style.transform = `translate(-${(window.pageYOffset - 7000) / 3}px, ${(window.pageYOffset - 7000) / 2}px)`;
+                imgSix.style.width = `${((window.pageYOffset - 7000) / 3) + 300}px`;
+                imgSix.style.height = `${((window.pageYOffset - 7000) / 3) + 200}px`;
+              }
+
+              // img7 anim
+              if (window.scrollY > 7500) {
+                imgSeven.style.display = "block";
+                imgSeven.style.opacity = (window.pageYOffset - 7500) / 500;
+                imgSeven.style.transform = `translate(${(window.pageYOffset - 7500) / 3}px, ${(window.pageYOffset - 7500) / 2}px)`;
+                imgSeven.style.width = `${((window.pageYOffset - 7500) / 3) + 300}px`;
+                imgSeven.style.height = `${((window.pageYOffset - 7500) / 3) + 200}px`;
+
+                // img8 anim
+                if (window.scrollY > 8000) {
+                  imgEight.style.display = "block";
+                  imgEight.style.opacity = (window.pageYOffset - 8000) / 500;
+                  imgEight.style.transform = `translate(-${(window.pageYOffset - 8000) / 3}px, ${(window.pageYOffset - 8000) / 2}px)`;
+                  imgEight.style.width = `${((window.pageYOffset - 8000) / 3) + 300}px`;
+                  imgEight.style.height = `${((window.pageYOffset - 8000) / 3) + 200}px`;
+                }
+                else imgEight.style.display = "none";
+              }
+              else imgSeven.style.display = "none";
+            }
+            else imgSix.style.display = "none";
+          }
+          else imgFive.style.display = "none";
+        }
+        else imgFour.style.display = "none";
+      }
+      else{
+        imgThree.style.display = "none";
+        imgTwo.style.display = "none";
+      }
+      
+    }
+
+    // display none images
+    else if (window.scrollY > 10000) {
+      imgOne.style.display = "none";
+      imgTwo.style.display = "none";
+      imgThree.style.display = "none";
+      imgFour.style.display = "none";
+      imgFive.style.display = "none";
+      imgSix.style.display = "none";
+      imgSeven.style.display = "none";
+      imgEight.style.display = "none";
+    }
     
 
+
+    
 
 
     console.log(window.scrollY);
@@ -99,7 +203,7 @@ const Portfolio = () => {
   
 
   return (
-    <div style={{height: "1000vh"}}>
+    <div style={{height: "2500vh"}}>
       <div className='banner-section'>
         <Banner />
         <OurGames />
